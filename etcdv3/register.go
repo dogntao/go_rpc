b4ce9b186e5f4c68a29a30b5384eee2ee615b5d5
dongt <dongt@dongtaodeMac-mini.local>
2020-03-20 20:25:49 +0800

    增加register

--- etcdv3/register.go
+++ etcdv3/register.go
@@ -1,6 +1,46 @@
 package etcdv3
 
-// 链接客户端
-func main() {
-	// clientv3.
+import (
+	"context"
+	"log"
+	"time"
+
+	clientv3 "github.com/coreos/etcd/clientv3"
+)
+
+type Service struct {
+	client *clientv3.Client
+}
+
+func NewService() (service *Service) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second,
+	})
+	defer cli.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	service = &Service{
+		client: cli,
+	}
+	return service
+}
+
+// 注册服务
+func (s *Service) Register(service, ip string) {
+	// 5秒超时
+	lease, err := s.client.Grant(context.TODO(), 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.client.Put(context.TODO(), service, ip, clientv3.WithLease(lease.ID))
+	// 自动续约
+	s.client.KeepAlive(context.TODO(), lease.ID)
+}
+
+// 获取服务
+func (s *Service) GetService(service string) (resp *clientv3.GetResponse) {
+	resp, _ = s.client.Get(context.TODO(), service)
+	return
 }
--- server/main.go
+++ server/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
 	gs "go_rpc/go_rpc"
 	"log"
 	"net"
+
+	etcd "go_rpc/etcdv3"
 
 	grpc "google.golang.org/grpc"
 )
 
 type server struct{}
+
+const (
+	ServiceName = "dt_go_rpc"
+	Ip          = "127.0.0.1:50051"
+)
 
 // 实现HelloWorld方法
 func (s *server) HelloWorld(ctx context.Context, req *gs.HelloRequest) (rep *gs.HelloResponse, err error) {
@@ -23,6 +31,10 @@ func (s *server) HelloWorld(ctx context.Context, req *gs.HelloRequest) (rep *gs.
 }
 
 func main() {
+	etcdService := etcd.NewService()
+	etcdService.Register(ServiceName, Ip)
+	fmt.Println(etcdService.GetService(ServiceName))
+
 	// 创建服务器监听
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
